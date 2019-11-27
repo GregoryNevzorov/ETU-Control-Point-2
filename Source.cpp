1b8c6f304989262812978f3f9642c2ed7566dde5
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-11-27 05:40:10 +0300

    1

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h> //Функция _getch() для считывания кода нажатых клавиш.
 #include <Windows.h> //Для настроек вывода в консоль и т.п.
+#include <stdlib.h> //Функция system("cls") для очистки консоли.
 
 using namespace std;
 
@@ -8,8 +9,77 @@ short int MainMenu()
 {
 	short int* marker = new short int(1);
 	short int* button_code = new short int;
-	*button_code = _getch();
-
+	while (true)
+	{
+		if (*marker == 1)
+		{
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+			cout << "1. Registration or Authorization\n";
+			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		}
+		else
+		{
+			cout << "1. Registration or Authorization\n";
+		}
+		if (*marker == 2)
+		{
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+			cout << "2. Greetings\n";
+			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		}
+		else
+		{
+			cout << "2. Greetings\n";
+		}
+		if (*marker == 3)
+		{
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+			cout << "3. Array\n";
+			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		}
+		else
+		{
+			cout << "3. Array\n";
+		}
+		if (*marker == 4)
+		{
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+			cout << "4. Flag\n";
+			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		}
+		else
+		{
+			cout << "4. Flag\n";
+		}
+		*button_code = _getch();
+		system("cls");
+		switch (*button_code)
+		{
+		case 72: //Стрелка вверх.
+		case 119: //W
+			*marker -= 1;
+			if (*marker < 1)
+			{
+				*marker = 4;
+			}
+			break;
+		case 80: //Стрелка вниз.
+		case 115: //S
+			*marker += 1;
+			if (*marker > 4)
+			{
+				*marker = 1;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
 }
 
 int main()
@@ -17,4 +87,3 @@ int main()
 	MainMenu();
 	return 0;
 }
-//тест
