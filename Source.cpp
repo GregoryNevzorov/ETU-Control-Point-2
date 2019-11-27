f0a41779b0ed483aa18c508e2bc757880d3f733b
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-11-27 05:09:07 +0300

    first

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,19 @@
-#include <iostream>;
+#include <iostream>
+#include <conio.h> //Функция _getch() для считывания кода нажатых клавиш.
+#include <Windows.h> //Для настроек вывода в консоль и т.п.
 
 using namespace std;
 
+short int MainMenu()
+{
+	short int* marker = new short int(1);
+	short int* button_code = new short int;
+	*button_code = _getch();
+
+}
+
 int main()
 {
-
+	MainMenu();
 	return 0;
 }
-//
